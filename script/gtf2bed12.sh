85d3b817f3a3da1c17254cf947ab3c549cf368f8
Czh3 <zhangchao3@hotmail.com>
2015-12-09 22:36:26 +0800

    gtf2bed

--- script/gtf2bed12.sh
+++ script/gtf2bed12.sh
@@ -1,3 +1,4 @@
+# gtfToGenePred were download form UCSC  : http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/gtfToGenePred
 gtfToGenePred -genePredExt -geneNameAsName2 genes.gtf gene.tmp
 awk '{print $2"\t"$4"\t"$5"\t"$1"\t0\t"$3"\t"$6"\t"$7"\t0\t"$8"\t"$9"\t"$10}' genes.tmp >  genes.bed12
 rm gene.tmp
